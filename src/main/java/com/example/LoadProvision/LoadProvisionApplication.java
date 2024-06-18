a65d4c52ac649d37883d10621c365e22c9731ffd
Binyam.Gebrekidan <binyam.gebrekidan@partner.safaricom.et>
2024-06-18 14:11:38 +0300

    first commit

---
+++ src/main/java/com/example/LoadProvision/LoadProvisionApplication.java
@@ -0,0 +1,13 @@
+package com.example.LoadProvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadProvisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadProvisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LoadProvision/LoadProvisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LoadProvision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadProvisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
